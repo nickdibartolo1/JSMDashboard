895a9aa0bc260d96f85d8e931c71f1e6582de604
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-01-30 20:33:38 -0500

    started router setup, installed graphql dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import { dataProvider, liveProvider } from "./providers";
+import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register } from './pages'
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier
@@ -27,6 +28,7 @@ function App() {
               liveProvider={liveProvider}
               notificationProvider={useNotificationProvider}
               routerProvider={routerBindings}
+              authProvider={authProvider}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
@@ -37,6 +39,10 @@ function App() {
             >
               <Routes>
                 <Route index element={<WelcomePage />} />
+                <Route index element={<Home />} />
+                <Route path='/register' index element={<Register/>} />
+                <Route path='/login' index element={<Login />} />
+                <Route path='/forgot-password' index element={<ForgotPassword />} />
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
