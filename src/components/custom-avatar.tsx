045e2b9c8db7fb1006b42709b976120942d0d3bf
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-01-31 17:50:57 -0500

    incorporating avatar

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,24 @@
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
+
+type Props = AvatarProps & {
+    name: string;
+}
+
+const CustomAvatar = ({ name, style, ...rest}: Props ) => {
+  return (
+    <AntdAvatar
+      alt={"Nick DiBartolo"}
+      size="small"
+      style={{
+        backgroundColor: "#93e074",
+        display: "flex",
+        alignItems: "center",
+        border: "none"
+      }}
+    >
+      {name}
+    </AntdAvatar>
+  );
+};
+
+export default CustomAvatar;
--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,18 +1,23 @@
-import { Popover } from 'antd'
+import { Popover } from "antd";
+import CustomAvatar from "../custom-avatar";
+import { useGetIdentity } from "@refinedev/core";
+
+import type { User } from '@/graphql/schema.types'
 
 const CurrentUser = () => {
-    return (
-        <>
-            <Popover 
-            placement='bottomRight' 
-            trigger="click" 
-            overlayInnerStyle={{ padding: 0 }}
-            overlayStyle={{ zIndex:999 }}
-            >
-sdasga
-            </Popover>
-        </>
-    )
-}
+    const { data: user } = useGetIdentity<User>();
+  return (
+    <>
+      <Popover
+        placement="bottomRight"
+        trigger="click"
+        overlayInnerStyle={{ padding: 0 }}
+        overlayStyle={{ zIndex: 999 }}
+      >
+        <CustomAvatar  />
+      </Popover>
+    </>
+  );
+};
 
-export default CurrentUser
+export default CurrentUser;
