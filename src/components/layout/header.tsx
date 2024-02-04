98556d9acd4837fda8c58090cdfa08c804828720
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-02-04 16:08:40 -0500

    styles for header, avatar

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,25 @@
-import CurrentUser from "./current-user"
+import { Layout, Space } from "antd";
+import CurrentUser from "./current-user";
+
+const headerStyle: React.CSSProperties = {
+  background: "#fff",
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  padding: "0 24px",
+  position: "sticky",
+  top: 0,
+  zIndex: 999
+};
 
 const Header = () => {
-    return (
-        <div>
-            <CurrentUser/>
-        </div>
-    )
-}
+  return (
+    <Layout.Header style={headerStyle}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
+  );
+};
 
 export default Header;
