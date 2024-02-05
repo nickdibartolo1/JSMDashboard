148c208266da32dde1c2a799352e720f612507ff
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-02-05 18:14:52 -0500

    edits in upcoming-events, added skeleton folder

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { CalendarOutlined } from "@ant-design/icons";
+import { Card, List } from "antd";
+import React, { useState } from "react";
+import { Text } from "../text";
 
 const UpcomingEvents = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [isLoading, setIsLoading] = useState(true);
 
-export default UpcomingEvents
+  return (
+    <Card
+      style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "0 1rem" }}
+      title={
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <CalendarOutlined />
+          <Text>Upcoming Events</Text>
+        </div>
+      }
+    >
+      {isLoading ? <List 
+      itemLayout="horizontal"
+      dataSource={Array.from({length: 5}).map((_, index) => ({id: index})
+      )}
+      ></List> : <List></List>}
+    </Card>
+  );
+};
+
+export default UpcomingEvents;
