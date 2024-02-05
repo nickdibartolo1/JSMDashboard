249d5b8f90614d16a24d240a1e50242a700ca3bc
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-02-05 17:59:34 -0500

    adding components to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { DealsChart, UpcomingEvents } from "@/components";
+import { Col, Row } from "antd";
+import React from "react";
 
 export const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "480px"
+          }}
+        >
+          <UpcomingEvents></UpcomingEvents>
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "480px"
+          }}
+        >
+          <DealsChart></DealsChart>
+        </Col>
+      </Row>
+    </div>
+  );
+};
